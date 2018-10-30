2f2afeb59be177866e4756ef8f44c3833a9d35da
Pedro Vazquez <pedrovazquezax@gmail.com>
2018-10-30 12:27:24 -0600

    rkunrk

---
+++ Segues/Segues/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  Segues
+//
+//  Created by MacBook on 25/10/18.
+//  Copyright © 2018 potato. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var item: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func unwindToYellow(unwindSegue: UIStoryboardSegue){
+        
+    }
+
+
+}
---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by MacBook on 25/10/18.
+//  Copyright © 2018 potato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+        
+    }
+
+    
+    
+    
+    
+
+}
+
